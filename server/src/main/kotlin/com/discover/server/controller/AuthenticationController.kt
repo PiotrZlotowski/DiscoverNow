b26159991117f0d6e893283e8d37aeec03991690
Piotr Zlotowski <piotrekzlotowski@gmail.com>
2018-05-28 19:18:41 +0200

    Modified detekt configuration

--- server/src/main/kotlin/com/discover/server/controller/AuthenticationController.kt
+++ server/src/main/kotlin/com/discover/server/controller/AuthenticationController.kt
@@ -7,18 +7,20 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.authentication.session.SessionAuthenticationException
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository
-import org.springframework.web.bind.annotation.*
-import java.util.ArrayList
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpSession
 
 @RestController
 @RequestMapping("/api/authorization/")
-class AuthenticationController constructor(private val authenticationManager : AuthenticationManager,
-                                           private val userDetailsService : UserDetailsService) {
-
-
+class AuthenticationController constructor(private val authenticationManager: AuthenticationManager,
+                                           private val userDetailsService: UserDetailsService) {
 
     @PostMapping("/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -28,21 +30,25 @@ class AuthenticationController constructor(private val authenticationManager : A
 
     @PostMapping("/authenticate")
     fun authorize(@RequestBody authenticationRequestRequest: AuthenticationRequest,
-                  httpServletRequest: HttpServletRequest) : AuthenticationToken {
+                  httpServletRequest: HttpServletRequest): AuthenticationToken {
+        val session = createHttpSession(authenticationRequestRequest, httpServletRequest)
+                ?: throw SessionAuthenticationException("Unable to create user session!")
+        return getAuthenticationToken(authenticationRequestRequest, session)
+    }
+
+    private fun getAuthenticationToken(authenticationRequestRequest: AuthenticationRequest, session: HttpSession): AuthenticationToken {
+        val details = this.userDetailsService.loadUserByUsername(authenticationRequestRequest.username)
+        val authToken = AuthenticationToken(username = details.username, sessionId = session.id)
+        return authToken
+    }
+
+    private fun createHttpSession(authenticationRequestRequest: AuthenticationRequest, httpServletRequest: HttpServletRequest): HttpSession? {
         val token = UsernamePasswordAuthenticationToken(authenticationRequestRequest.username, authenticationRequestRequest.password)
         val authentication = this.authenticationManager.authenticate(token)
         SecurityContextHolder.getContext().authentication = authentication
         val session = httpServletRequest.getSession(true)
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext())
-
-        val details = this.userDetailsService.loadUserByUsername(authenticationRequestRequest.username)
-        val roles = ArrayList<String>()
-        for (authority in details.authorities) {
-            roles.add(authority.toString())
-        }
-
-        val authToken = AuthenticationToken(username = details.username, sessionId = session.id)
-        return authToken
+        return session
     }
 
 }
