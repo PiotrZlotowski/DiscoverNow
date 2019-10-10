d26f8f9ae2c4ba8f98ccf26eb11f8c9bb4cfc2dc
Piotr Zlotowski <piotrekzlotowski@gmail.com>
2019-10-10 22:04:20 +0200

    Add another two ArchUnit tests

--- server/src/test/kotlin/com/discover/server/architecture/ArchitectureTests.kt
+++ server/src/test/kotlin/com/discover/server/architecture/ArchitectureTests.kt
@@ -1,14 +1,20 @@
 package com.discover.server.architecture
 
 import com.discover.server.ServerApplication
+import com.tngtech.archunit.core.domain.JavaConstructor
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.ArchCondition
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
+import com.tngtech.archunit.lang.SimpleConditionEvent
+import com.tngtech.archunit.lang.ConditionEvents
+import javax.persistence.Entity
 
 
 @AnalyzeClasses(packagesOf = [ServerApplication::class])
@@ -31,5 +37,23 @@ class ArchitectureTests {
             noClasses().that().arePublic()
                     .or().areNotPublic()
                     .should().resideInAnyPackage("..controller..","..service..", "..repository..", "..dto..")
+    @ArchTest
+    val constructors_annotated_with_spring_annotations_should_not_be_annotated_with_Autowired =
+            constructors().that().areDeclaredInClassesThat().areAnnotatedWith(Service::class.java)
+                    .or().areAnnotatedWith(Repository::class.java)
+                    .or().areAnnotatedWith(Component::class.java)
+                    .should().notBeAnnotatedWith(Autowired::class.java)
 
+    private var haveNoMoreThanSixArguments= object : ArchCondition<JavaConstructor>("be slim") {
+        override fun check(constructor: JavaConstructor, events: ConditionEvents) {
+            val haveMoreThanSixArguments = constructor.reflect().parameterTypes.size <= 6
+            val message = "${constructor.fullName} contains more than six arguments"
+            events.add(SimpleConditionEvent(constructor, haveMoreThanSixArguments, message))
+        }
+    }
+
+    @ArchTest
+    val constructor_must_not_have_more_than_six_arguments =
+            constructors().that().areDeclaredInClassesThat().areNotAnnotatedWith(Entity::class.java)
+                    .should(haveNoMoreThanSixArguments)
 }
